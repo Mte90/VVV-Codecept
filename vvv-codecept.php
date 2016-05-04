3b9b7ce2c38fa5ea9c0b196e34d4e98cb0ac9983
Daniele Scasciafratte <mte90net@gmail.com>
2016-05-04 19:43:19 +0200

    create also a wpconfig file

--- vvv-codecept.php
+++ vvv-codecept.php
@@ -81,6 +81,7 @@ modules:
             title: 'WP Tests'
             phpBinary: php
             language: ''
+            configFile: 'wp-config-test.php'
             plugins: [\"".$path[4]."/".$path[4].".php\"]
             activatePlugins: [\"".$path[4]."/".$path[4].".php\"]
         WPWebDriver:
@@ -95,6 +96,33 @@ modules:
 ";
 
 file_put_contents('codeception.yml', $codeception);
+
+$wpconfig = "
+<?php
+
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress_unit_tests');
+
+/** MySQL database username */
+define('DB_USER', 'wp');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wp');
+
+/** MySQL hostname */
+define('DB_HOST', '192.168.50.4');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+$"."table_prefix = 'wp_';
+";
+
+file_put_contents('wp-config-test.php', $codeception);
 
 $db = str_replace('domain-replace', $path[0], file_get_contents('https://raw.githubusercontent.com/Mte90/VVV-Codecept/master/dump.sql'));
 file_put_contents('tests/_data/dump.sql', $db);
